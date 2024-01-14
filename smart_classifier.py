7254f74705d30c79bf04af1a02c4328c02f24273
NSAandYou <development@sprepo.com>
2024-01-14 12:03:12 +0100

    1.10 - style

--- smart_classifier.py
+++ smart_classifier.py
@@ -18,5 +18,5 @@ def predict(pkt):
     if len(pkt_bytes) > MAX_FEATURE_COLUMNS:
         pkt_bytes = pkt_bytes[:MAX_FEATURE_COLUMNS]
     features[0:len(pkt_bytes)] = np.frombuffer(pkt_bytes, np.int8)
-    features = np.delete(features, [6, 7, 8, 9, 10, 11, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,32,33,34,35,36,37])
+    features = np.delete(features, [6, 7, 8, 9, 10, 11, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37])
     return clf.predict(features.reshape(1, -1))[0]
